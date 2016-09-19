e442d32c4696898328d0e60a2101cfb8ec7ea2fa
Dima Papirnyi <dima77777work@mail.ru>
2016-09-19 21:01:11 +0300

    refactoring

--- build/bundle.js
+++ build/bundle.js
@@ -413,24 +413,16 @@
 	});
 
 	app.controller('mainController', function ($scope, dataService) {
-		$scope.src = "http://ia.media-imdb.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg";
 		$scope.show = false;
-		$scope.apiKey = '402c455d';
-		$scope.imdbID = 'tt2294629';
-		$scope.mediaLink = 'http://img.omdbapi.com/?i=' + $scope.imdbID + '&apikey=' + $scope.apiKey;
-		console.log($scope.mediaLink);
 		$scope.media = null;
 		$scope.desired = {};
 		$scope.searchFunc = function (input) {
 			dataService.getJSON(function (response, input) {
 				$scope.movies = response.data.Search;
-				console.log(response.data);
 			}, input);
 		};
 		$scope.addToDesired = function (movie, title) {
-			console.log('desired', title);
 			localStorage.setItem(title, null);
-
 			$scope.desired.push(movie);
 		};
 	});
